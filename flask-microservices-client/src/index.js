72716898e70ffba3ee44496b89d8cddab0bad383
bicyclesforthemind <aaa2141@gmail.com>
2017-09-16 09:57:56 -0500

    Modified and containerized react client

--- flask-microservices-client/src/index.js
+++ flask-microservices-client/src/index.js
@@ -2,24 +2,43 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 import registerServiceWorker from './registerServiceWorker';
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {users: []};
+        this.state = {users: [], username: '', email: ''};
     }
 
     componentDidMount() {
         this.getUsers();
     }
 
-    getUsers() {
-        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`).then((response) => {
-            this.setState({ users: response.data.data.users });
+    getUsers(event) {
+        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+        .then((res) => { this.setState({ users: res.data.data.users }); })
+        .catch((err) => { console.log(err); })
+    }
+
+    addUser(event) {
+        event.preventDefault();
+        const data = {
+            username: this.state.username,
+            email: this.state.email
+        };
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data).then((response) => {
+            this.getUsers();
+            this.setState({username: '', email: ''});
         }).catch((error) => {
             console.log(error);
         });
+    }
+
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
     }
 
     render() {
@@ -30,6 +49,8 @@ class App extends Component {
                 <br/>
                 <h1>All Users</h1>
                 <hr/><br/>
+                <AddUser username={this.state.username} email={this.state.email} addUser={this.addUser.bind(this)} handleChange={this.handleChange.bind(this)}/>
+                <br/>
                 <UsersList users={ this.state.users } />
               </div>
             </div>
